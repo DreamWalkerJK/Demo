e5dd959ba36798addbd273e811263218d99babe9
DreamWalkerJK <lunatic_jk@qq.com>
2023-09-09 22:36:09 +0800

    20230909 SignalR Blazor && gRPC

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var channel = GrpcChannel.ForAddress("https://localhost:7251"))
+            {
+                var client = new Greeter.GreeterClient(channel);
+
+                var reply = client.SayHello(
+                    new HelloRequest { Name = "GreeterClient" });
+
+                Console.WriteLine("Greeting: " + reply.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
